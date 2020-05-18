87dce97f9d9b928f460ef9acd724aac5726c42f8
Martypose <46140279+Martypose@users.noreply.github.com>
2020-05-18 15:51:25 +0200

    Get
    Get

--- madEst/routes/paquetes.js
+++ madEst/routes/paquetes.js
@@ -8,29 +8,37 @@ router.post('/', function(req, res) {
 
     let paquete = req.body.paquete
 
-    //Aquí debería guardar el paquete que recibo en la base de datos.
-
-    dbConn.query('INSERT INTO paquete SET id=?, fechaCreacion=?, seco=?, estado=?, barroteado=?, homogeneo=?, grosor=?, largo=?, cubico=?, numpiezas=?, calidad=?', [paquete.id, paquete.fecha, paquete.seco, paquete.estado,paquete.barroteado, paquete.homogeneo, paquete.grosor, paquete.largo, paquete.cubico, paquete.numpiezas, paquete.calidad], function(err, result) {
+    //Guardar paquete
+    dbConn.query('INSERT INTO paquete SET id=?, fechaCreacion=?, seco=?, estado=?, barroteado=?, homogeneo=?, cantidades=?, grosor=?, largo=?, cubico=?, numpiezas=?, calidad=?', [paquete.id, paquete.fecha, paquete.seco, paquete.estado,paquete.barroteado, paquete.homogeneo,paquete.cantidades, paquete.grosor, paquete.largo, paquete.cubico, paquete.numpiezas, paquete.calidad], function(err, result) {
         //if(err) throw err
         if (err) {
-          console.log("Error en el insert"+ err)
+          console.log("Error en el insert "+ err)
         } else {
             //mostrar mensaje éxito
             console.log('exito al guardar en bd');
             res.send('exito al guardar en bd');
         }
       })
-
-
+    
+  
 })
 
-
+//Si hacemos un get
 router.get('/', function(req,res){
-    if(req){
+  //Dos opciones, paquetes bajados o no.
+  //Hago una consulta a la BD con el parámetro que me llega para elegir un o otro tipo
+  //En este caso según ->barroteado:
+
+  dbConn.query(`SELECT * FROM paquete WHERE barroteado=${req.query.barroteado}`, function (err, result, fields) {
+    if (err) {
+      console.log('Error en la consulta a la bd '+ err)
     }
-    //Aqui obtengo los datos de los paquetes que deseo mostrar y los envio
+    console.log(result);
+    res.json(result);
+  });
+  //Enviar resultado en forma de JSON
 
-
+  
 })
 
 module.exports = router
