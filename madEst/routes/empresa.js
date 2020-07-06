decbf27293d8ca89aa8a97906e26d9545c16f1ce
Martypose <martinpose@hotmail.com>
2020-07-06 23:39:54 +0000

    Cambios

--- madEst/routes/empresa.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET empresa page. */
-router.get('/', function(req, res, next) {
-  res.render('empresa');
-});
-
-module.exports = router;
--- madEst/routes/serrado.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET serrado page. */
-router.get('/', function(req, res, next) {
-  res.render('serrado');
-});
-
-module.exports = router;
