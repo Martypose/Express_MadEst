39187b31e1d5d7e10a31c3aa95a3d61919fc9f5d
Martypose <46140279+Martypose@users.noreply.github.com>
2020-07-07 18:11:49 +0200

    instalación modulos y inicio login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET index page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express'});
+  res.render('login');
 });
 
 module.exports = router;
