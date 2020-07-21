2cb9d4cd812a684fef012082af8862207d2b1dad
Martypose <46140279+Martypose@users.noreply.github.com>
2020-07-21 17:27:36 +0200

    Update medidas.js

--- routes/medidas.js
+++ routes/medidas.js
@@ -32,9 +32,9 @@ router.get('/',function(req,res){
   })
 
   router.delete('/:id',function(req,res){
-    console.log(`intentando borrar ${req.query.id}`)
+    console.log(`intentando borrar ${request.params.id}`)
   
-    dbConn.query('DELETE FROM medidas WHERE id=?',[req.query.id], function (err, result) {
+    dbConn.query('DELETE FROM medidas WHERE id=?',[request.params.id], function (err, result) {
       if (err) {
         console.log('Error en el borrado'+ err)
       }
