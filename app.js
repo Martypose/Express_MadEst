358f0713125a091489faae7920a123451566c9e7
Martypose <46140279+Martypose@users.noreply.github.com>
2020-08-03 09:02:48 +0200

    añadir cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 //Importamos rutas
 const indexRouter = require('./routes/index');
@@ -26,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 
 //Rutas que usamos, y que acontece en cada una está dentro de cada archivo
 app.use('/', indexRouter);
