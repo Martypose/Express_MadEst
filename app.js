6bf94673b860eea5501a94069d0b335f5f555545
Martypose <46140279+Martypose@users.noreply.github.com>
2020-08-11 10:11:54 +0200

    intento seguridad public

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
 const cors = require('cors');
 
 //Importamos rutas
@@ -17,6 +18,8 @@ const transporteRouter = require('./routes/transporte');
 const loginRouter = require('./routes/login');
 const medidasRouter = require('./routes/medidas');
 
+const seguridad = require('../lib/seguridad')
+
 
 const app = express();
 
@@ -27,7 +30,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(seguridad,express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 //Rutas que usamos, y que acontece en cada una está dentro de cada archivo
