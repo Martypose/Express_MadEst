bbc5323d2059e436aff6d758616a341cf8a4ac4b
Martypose <46140279+Martypose@users.noreply.github.com>
2020-08-20 16:04:59 +0200

    Update paquetes.js

--- routes/paquetes.js
+++ routes/paquetes.js
@@ -30,7 +30,7 @@ router.post('/',seguridad, function(req, res) {
                 console.log('Error en la consulta a la bd '+ err)
               }
               //Enviar resultado en forma de JSON
-              res.send(result[0]);
+              res.send(result[0].ID);
             });
         }
       })
