79672002faa0ba20600caf6768ae2237426f3b3a
Martypose <46140279+Martypose@users.noreply.github.com>
2020-09-10 08:39:33 +0200

    añadiendo clientes

--- routes/clientes.js
+++ routes/clientes.js
@@ -7,7 +7,7 @@ var dbConn  = require('../lib/db');
 //Si hacemos un get
 router.get('/',seguridad, function(req,res){
 
-  dbConn.query(`Select *  from cliente;`, function (err, result, fields) {
+  dbConn.query(`Select * from cliente;`, function (err, result, fields) {
     if (err) {
       console.log('Error en la consulta a la bd'+ err)
     }
