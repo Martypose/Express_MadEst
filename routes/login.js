3646cfd89ad19be391c4a14ab5ae65fd3d4fa8c4
Martypose <martinpose@hotmail.com>
2022-04-21 02:31:51 +0200

    Eliminar node_modules y mejorar login

--- routes/login.js
+++ routes/login.js
@@ -2,17 +2,20 @@ const express = require('express');
 const seguridad = require('../lib/seguridad')
 const router = express.Router();
 const dbConn  = require('../lib/db')
+const mysql = require("mysql")
+const bcrypt = require("bcrypt")
 
 //postalogin
-router.post('/', function(req, res) {
+router.post('/', async function(req, res) {
+  
     const username = req.body.name
     const password = req.body.password
 
     const sqlSearch = "Select * from usuarios where username = ?"
     const search_query = mysql.format(sqlSearch,[username])
+    console.log(username)
 
     await dbConn.query (search_query, async (err, result) => {
-      dbConn.release()
      
      if (err) throw (err)
      if (result.length == 0) {
@@ -23,7 +26,7 @@ router.post('/', function(req, res) {
         //get the hashedPassword from result
        if (await bcrypt.compare(password, hashedPassword)) {
        console.log("---------> Login Successful")
-       res.send(`${user} is logged in!`)
+       res.send(`${username} is logged in!`)
        } 
        else {
        console.log("---------> Password Incorrect")
