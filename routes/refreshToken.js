4e4f84a6bb3412814924c6f8a247e0ecc706ef3d
Martypose <martinpose@hotmail.com>
2024-06-20 22:45:02 +0200

    less log

--- routes/refreshToken.js
+++ routes/refreshToken.js
@@ -1,39 +1,45 @@
-const express = require('express')
-const router = express.Router()
-const jwt = require('jsonwebtoken')
-require('dotenv').config();
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-//logear usuario
-router.get('/', async function(req, res) {
-    console.log('peticion recibida de nuevo token,refreeshhhh')
-    const refreshtoken = req.header('refreshToken')
-    const username = req.header('username')
-    console.log(refreshtoken)
-if (!refreshtoken) return res.status(401).json({ error: 'Acceso denegado' })
-try {
-    console.log(process.env.TOKEN_REFRESH_SECRET)
-    const verified = jwt.verify(refreshtoken, process.env.TOKEN_REFRESH_SECRET)
-    req.user = verified
-    //enviamos accesToken, refreshToken y username
-    const accessToken = jwt.sign({
-        name: username
-      }, process.env.TOKEN_SECRET, {expiresIn: process.env.JWT_ACCESS_EXPIRATION})
-  
-      const refreshToken = jwt.sign({
-        name: username
-      }, process.env.TOKEN_REFRESH_SECRET, { expiresIn: process.env.JWT_REFRESH_EXPIRATION})
-  
-         console.log("reenviamosTokensfrescos")
-         res.header('autorizado').json({
-          accessToken: {accessToken},
-          refreshToken: {refreshToken},
-          username: username
-        })
+// Ruta para renovar el token
+router.get("/", async function (req, res) {
+  console.log("Petición recibida para renovar el token");
 
-} catch (error) {
-    res.status(400).json({error: 'refreshToken no es válido'})
-    console.log(error)
-}
+  const refreshToken = req.header("refreshToken");
+  const username = req.header("username");
+
+  if (!refreshToken) return res.status(401).json({ error: "Acceso denegado" });
+
+  try {
+    const verified = jwt.verify(refreshToken, process.env.TOKEN_REFRESH_SECRET);
+    req.user = verified;
+
+    // Generar nuevos tokens
+    const newAccessToken = jwt.sign(
+      { name: username },
+      process.env.TOKEN_SECRET,
+      { expiresIn: parseInt(process.env.JWT_ACCESS_EXPIRATION, 10) } // Asegúrate de que el valor sea un número
+    );
+
+    const newRefreshToken = jwt.sign(
+      { name: username },
+      process.env.TOKEN_REFRESH_SECRET,
+      { expiresIn: parseInt(process.env.JWT_REFRESH_EXPIRATION, 10) } // Asegúrate de que el valor sea un número
+    );
+
+    console.log("Tokens renovados correctamente");
+
+    res.json({
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
+      username: username,
+    });
+  } catch (error) {
+    console.error("Error al verificar el refreshToken:", error);
+    res.status(400).json({ error: "refreshToken no es válido" });
+  }
 });
 
 module.exports = router;
